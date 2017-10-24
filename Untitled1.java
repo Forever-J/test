f35d39740d68e2421d6b7d73b4544501b2b88007
Forever-J <foreverj.2012@foxmail.com>
2017-10-25 01:52:01 +0800

    20171025

---
+++ Untitled1.java
@@ -0,0 +1,18 @@
+public class Num2Rmb
+{
+	private String[] hanArr={"Áã","Ò¼","·¡","Èş","ËÁ","Îé","Â½","Æâ","°Æ","¾Á"};
+	private String[] unitArr={"Ê°","°Û","Çª","Íò","ÒÚ"};
+
+	private String[] divide(double num){}
+
+	private String[] toHanStr(String numStr)
+	{
+		int numLen=numStr.length();
+		for(int i=0;i<numLen;i++)
+		{
+			int
+		}
+	}
+
+	
+}
