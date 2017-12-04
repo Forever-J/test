24e454ad503a1a49d83cf5c9da058cd1f18f47f3
Forever-J <foreverj.2012@foxmail.com>
2017-12-04 23:56:06 +0800

    20171204

---
+++ OutputFactory.java
@@ -0,0 +1,14 @@
+public class OutputFactory
+{
+	public Output getOutput()
+	{
+		return new Printer();
+	}
+	public static void main(String[] args)
+	{
+		OutputFactory of=new OutputFactory();
+		Computer c=new Computer(of.getOutput());
+		c.keyIn("简单工厂模式练习");
+		c.print();
+	}
+}
