6069ffbed78bc92a6872822b24eac2160ce9e87e
姜汉平 jianghanping () <jianghanping@tuniu-inc.com>
2017-12-13 19:16:28 +0800

    1213

---
+++ CommandTest.java
@@ -0,0 +1,12 @@
+public class  CommandTest
+{
+	public static void main(String[] args) 
+	{
+		ProcessArray pa=new ProcessArray();
+		int[] target={3,9,-8,5};
+		pa.process(target,new PrintCommand());
+		System.out.println("----------------");
+		pa.process(target,new AddCommand());
+		System.out.println("Hello World!");
+	}
+}
--- OutputFactory.java
+++ OutputFactory.java
@@ -3,14 +3,10 @@ import j.Output;
 
 public class OutputFactory
 {
-	public Output getOutput()
-	{
-		return new Printer();
-	}
+
 	public static void main(String[] args)
 	{
-		OutputFactory of=new OutputFactory();
-		Computer c=new Computer(of.getOutput());
+		Computer c=new Computer(new printer());
 		c.keyIn("简单工厂模式练习");
 		c.keyIn("简单工厂模式练习");
 		c.print();
