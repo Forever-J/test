a18cd699c7e435a4ea27422684af7616f43f62b1
Forever-J <foreverj.2012@foxmail.com>
2017-12-19 01:39:46 +0800

    12191

---
+++ innerClass/Cow.java
@@ -0,0 +1,35 @@
+public class Cow
+{
+	private double weight;
+	private Cow(){}
+	private Cow(double weight)
+	{
+		this.weight=weight;
+	}
+	private class CowLeg
+	{
+		private double length;
+		private String color;
+		public CowLeg(){}
+		public CowLeg(double length,String color)
+		{
+			this.length=length;
+			this.color=color;
+		}
+		public void info()
+		{
+			System.out.println("当前牛腿的颜色是："+color+",高"+length);
+			System.out.println("本牛腿所在奶牛重："+weight);
+		}
+	}
+	public void test()
+	{
+		CowLeg cl=new CowLeg(1.12,"黑白相间");
+		cl.info();
+	}
+	public static void main(String[] args)
+	{
+		Cow cow=new Cow(378.9);
+		cow.test();
+	}
+}
