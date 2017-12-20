26c833a5f2a4e84bf7e50bbf49907919c7f3ea01
Forever-J <foreverj.2012@foxmail.com>
2017-12-21 02:23:19 +0800

    12211

---
+++ innerClass/AnonymousTest.java
@@ -0,0 +1,27 @@
+interface Product
+{
+	public double getPrice();
+	public String getName();
+}
+public class AnonymousTest
+{
+	public void test(Product p)
+	{
+		System.out.println("购买了一个"+p.getName()+",花掉了"+p.getPrice());
+	}
+	public static void main(String[] args)
+	{
+		AnonymousTest ta=new AnonymousTest();
+		ta.test(new Product()
+		{
+			public double getPrice()
+			{
+				return 567.8;
+			}
+			public String getName()
+			{
+				return "AGP显卡";
+			}
+		});
+	}
+}
