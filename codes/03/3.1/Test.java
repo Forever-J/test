b91d205c37b4b28f39abb24ccaccaa02b27f4c84
Forever-J <foreverj.2012@foxmail.com>
2018-05-15 02:07:45 +0800

    20180515

---
+++ codes/03/3.1/Test.java
@@ -0,0 +1,26 @@
+package yeeku;
+
+/**
+ * Description:
+ * <br>???: <a href="http://www.crazyit.org">???Java????</a>
+ * <br>Copyright (C), 2001-2016, Yeeku.H.Lee
+ * <br>This program is protected by copyright laws.
+ * <br>Program Name:
+ * <br>Date:
+ * @author Yeeku.H.Lee kongyeeku@163.com
+ * @version 1.0
+ */
+public class Test
+{
+	/**
+	 * ???????????
+	 */
+	public int age;
+	/**
+	 * Test???????????
+	 */
+	public Test()
+	{
+	}
+}
+
